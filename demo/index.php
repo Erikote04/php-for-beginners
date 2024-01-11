4a2029a31ba255c9778a7e316d160ebaebf9b558
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-11 18:01:05 +0100

    Conditionals and booleans

--- demo/index.php
+++ demo/index.php
@@ -4,15 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 
 <body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You have NOT read $name";
+    }
+    ?>
+
     <h1>
-        <?php
-        $greeting = "Hello";
-        echo "$greeting World"; // Single quotes can't read values of variables
-        ?>
+        <?= $message ?>
     </h1>
 </body>
 
