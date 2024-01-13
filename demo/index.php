222b51732db30873a62369d2633e74f2164b6a64
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-13 22:30:20 +0100

    Functions and filters

--- demo/index.php
+++ demo/index.php
@@ -13,20 +13,39 @@
         [
             'title' => '12 Rules for Life',
             'author' => 'Jordan B. Peterson',
+            'releaseYear' => 2018,
+            'purchaseURL' => 'https://example.com'
+        ],
+        [
+            'title' => 'Beyond Order',
+            'author' => 'Jordan B. Peterson',
+            'releaseYear' => 2021,
             'purchaseURL' => 'https://example.com'
         ],
         [
             'title' => 'Atomic Habits',
             'author' => 'James Clear',
+            'releaseYear' => 2018,
             'purchaseURL' => 'https://example.com'
         ],
-    ]
-        ?>
+    ];
+
+    function filterByAuthor($books, $author) {
+        $filteredBooks = [];
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        return $filteredBooks;
+    }
+    ?>
+
     <ul>
-        <?php foreach ($books as $book): ?>
+        <?php foreach (filterByAuthor($books, 'Jordan B. Peterson') as $book): ?>
             <li>
                 <a href="<?= $book['purchaseURL'] ?>">
-                    <?= $book['title'] ?>
+                    <?= $book['title'] ?> (<?= $book['releaseYear'] ?>) 
                 </a>
             </li>
         <?php endforeach ?>
