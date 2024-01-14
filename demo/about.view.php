61b74de83630cb8806658fd8c93f6c2c0b941a51
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-14 14:02:27 +0100

    [Dynamic Web Application] About page

--- demo/about.view.php
+++ demo/about.view.php
@@ -107,7 +107,7 @@
 
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                Welcome to the home page
+                Now you are on the About page
             </div>
         </main>
     </div>
--- demo/index.php
+++ demo/index.php
@@ -1,38 +1,3 @@
 <?php
-$books = [
-    [
-        'title' => '12 Rules for Life',
-        'author' => 'Jordan B. Peterson',
-        'releaseYear' => 2018,
-        'purchaseURL' => 'https://example.com'
-    ],
-    [
-        'title' => 'Beyond Order',
-        'author' => 'Jordan B. Peterson',
-        'releaseYear' => 2021,
-        'purchaseURL' => 'https://example.com'
-    ],
-    [
-        'title' => 'Atomic Habits',
-        'author' => 'James Clear',
-        'releaseYear' => 2018,
-        'purchaseURL' => 'https://example.com'
-    ],
-];
-
-function filter($items, $fn)
-{
-    $filteredItems = [];
-    foreach ($items as $item) {
-        if ($fn($item)) {
-            $filteredItems[] = $item;
-        }
-    }
-    return $filteredItems;
-}
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['author'] === 'Jordan B. Peterson';
-});
 
 require "index.view.php";
